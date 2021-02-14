e2c7443b579fc9d5153148bf22bd8db1097cc5fa
alucard <faalsaru@gmail.com>
2021-02-14 18:12:17 -0500

    Temporización y contador de FPS
    cap 05, sistema de control de actualizaciones por segundos y frames por segundo, además de contador en la barra de la ventana

--- src/game/Game.java
+++ src/game/Game.java
@@ -17,6 +17,10 @@ public class Game extends Canvas implements Runnable
     private static final int HEIGHT = 600;
 
     private static final String NAME = "Juego";
+    
+    private static int aps = 0;
+    private static int fps = 0;
+    
 
     private static JFrame window;
 
@@ -62,12 +66,58 @@ public class Game extends Canvas implements Runnable
 		}
     }
     
+    private void refresh() 
+    {
+    	aps++;
+    }
+    
+    private void showGraphics() {
+    	fps++;
+    }
+    
+    
+    
 	@Override
 	public void run() {
 		System.out.println("ejecutando...");
+		
+		final int NS_POR_SEGUNDO = 1000000000;
+		final byte APS_OBJETIVO = 60; 
+		final double NS_POR_ACTUALIZACION = NS_POR_SEGUNDO / APS_OBJETIVO;
+		
+		long referenciaActualizacion = System.nanoTime();
+		
+		long referenciaContador = System.nanoTime();
+		
+		double tiempoTranscurrido;
+		double delta = 0;
+		
+		
+		
 		while(running) 
 		{
+			final long inicioBucle = System.nanoTime();
 			
+			tiempoTranscurrido = inicioBucle - referenciaActualizacion;
+			referenciaActualizacion = inicioBucle;
+			
+			delta += tiempoTranscurrido / NS_POR_ACTUALIZACION;
+			
+			while (delta >= 1) {
+				this.refresh();
+				delta--;
+			}
+			
+			
+			this.showGraphics(); 
+			
+			if(System.nanoTime() - referenciaContador > NS_POR_SEGUNDO) 
+			{
+				window.setTitle(NAME + " || APS: " + aps + " || FPS: " + fps);
+				aps = 0;
+				fps = 0;
+				referenciaContador = System.nanoTime();
+			}
 		}
 	}
 }
